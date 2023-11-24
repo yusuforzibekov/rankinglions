a51449a7c5f4cce2d57117eec84c4d73711e315a
Orzibekov Yusufbek <orzibekovyusufbek@gmail.com>
2023-11-25 01:10:49 +0500

    Refactor header burger functionality and update styles

--- js/script.js
+++ js/script.js
@@ -1,5 +1,24 @@
 const burger = document.querySelector('.header-burger');
 const nav = document.querySelector('.navbar');
+const items = document.querySelectorAll('.header-burger-items');
+let open = document.querySelector('#open');
+let writer = document.querySelector('#writer');
+
+
+let str = writer.innerText;
+writer.innerHTML = '';
+let count = 0;
+let timer;
+
+function print() {
+    writer.innerHTML += str.charAt(count);
+    count++;
+    if (str.length == count) {
+        clearInterval(timer);
+    }
+}
+
+timer = setInterval(print, 250);
 
 burger.addEventListener('click', () => {
     // Toggle the 'open' class on the 'nav' element
@@ -7,5 +26,12 @@ burger.addEventListener('click', () => {
         nav.id = ''; // Remove the ID
     } else {
         nav.id = 'open'; // Set the ID
+    }
+    items.forEach(item => item.classList.toggle('ec'));
+
+    if (nav.id === 'open') {
+        document.body.style.overflowY = 'hidden';
+    } else {
+        document.body.style.overflowY = '';
     }
 });
